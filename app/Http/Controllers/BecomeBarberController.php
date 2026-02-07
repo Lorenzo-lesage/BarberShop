8a8a91ea853f1bd7448990a7307384e179bce650
lorenzo lesage <lollolesage99@gmail.com>
2026-02-07 12:42:06 +0100

    Fix flash toast using back() instead of redirect()->back()

--- app/Http/Controllers/BecomeBarberController.php
+++ app/Http/Controllers/BecomeBarberController.php
@@ -41,7 +41,7 @@ class BecomeBarberController extends Controller
             new BarberRequestMail($user, $approveUrl)
         );
 
-        return redirect()->back()->with('toast', [
+        return back()->with('toast', [
             'type' => 'success',
             'message' => 'Request sent!',
             'description' => 'We will review your request soon.',
